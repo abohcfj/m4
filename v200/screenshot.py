509a0f44fd0c6265493071a62167d556fa40c110
zangbei <1300710614@qq.com>
2024-05-17 17:34:49 +0800

    修改颜色相近代码

--- v200/screenshot.py
+++ v200/screenshot.py
@@ -3,32 +3,22 @@ import cv2
 import time
 import numpy as np
 from PIL import Image
+import numpy as np
 
-def check_color(image, target_color, tolerance):
-    # 获取图像的宽度和高度
-    width, height = image.size
-    
-    # 循环遍历图像的每个像素
-    for y in range(height):
-        for x in range(width):
-            # 获取当前像素的颜色
-            current_color = image.getpixel((x, y))
-            # 检查当前像素的颜色是否在容差范围内
-            if all(abs(c1 - c2) <= tolerance for c1, c2 in zip(current_color, target_color)):
-                return True
-    # 如果没有找到目标颜色，则返回False
-    return False
-image = Image.open("obj.png")  # 替换为你的图像路径
-print(image)
-color_exists = check_color(image,(255,0,0),20)
+def rgb_distance(rgb1, rgb2):
+    return np.sqrt(np.sum((rgb1 - rgb2) ** 2))
+
+target_rgb =[255,0,0]
+threshold = 20
+camera = dxcam.create(output_idx=0)
+camera.start(target_fps=120)
+image = camera.get_latest_frame()
+image_data = np.array(image)
+color_exists = np.any(np.linalg.norm(image_data - target_rgb, axis=-1) <= threshold)
 if color_exists:
     print("图像中存在与目标颜色相近的颜色。")
 else:
     print("图像中不存在与目标颜色相近的颜色。")
-camera = dxcam.create(output_idx=0)
-camera.start(target_fps=120)
-image1 = camera.get_latest_frame()
-print(image1)
 camera.stop()
 # while True:
 #     image = camera.get_latest_frame()
